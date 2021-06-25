c0f7224137a7c3f9550c9503ceb34191b1188860
zhangpengfei <zhangpengfei@zerosportsai.com>
2021-06-25 17:20:21 +0800

    fix

--- src/main/java/com/example/demo1/Pointer.java
+++ src/main/java/com/example/demo1/Pointer.java
@@ -15,6 +15,9 @@ public class Pointer {
         int[] arr = {1,2,3,4,5};
         int i = maxi(arr, 11);
         System.out.println(i);
+        System.out.println(i);
+        System.out.println(i);
+
     }
 
     static int maxi(int[] nums, int target) {
